be5338f6366aee0cd8e8f4cf9a4214837f0c6dbf
rimfat1311 <simontimnan@gmail.com>
2024-10-04 16:26:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import AboutUs from "./components/about";
+import Users from "./components/users";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/users" element={<Users />} />
       </Routes>
     </div>
   );
