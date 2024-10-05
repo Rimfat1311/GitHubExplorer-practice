8a5dad0fb293ec91ebea36807791b83382cfdade
rimfat1311 <simontimnan@gmail.com>
2024-10-04 17:42:27 -0700

    update

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Home from "./components/home";
 import AboutUs from "./components/about";
 import Users from "./components/users";
 import Navbar from "./components/navbar";
+import Notfound from "./components/notfound"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navbar />}> <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/users" element={<Users />} /></Route>
-       
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/users" element={<Users />} />
+        </Route>
+        <Route path="*" element={<Notfound />} />
       </Routes>
     </div>
   );
